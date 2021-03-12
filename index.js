107c8eda2b8a9909897d369e8ba591b0847dc846
Sabihuddin Ahmed Siddiqui <suas2008hm@hotmail.com>
2021-03-13 00:16:27 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+//const express=require('express');
+import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/users.js';
+
+
+const app=express();
+const PORT=5000;
+
+app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
+
+app.get('/',(req, res) => res.send('Hello from Homepage'));
+
+app.listen(PORT, () => console.log(`Server is Running on port: http://localhost:${PORT}`));
+
